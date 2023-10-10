f426ee46f903706588f262778bef724b231bcf4e
Jay <me@jayxtq.xyz>
2023-10-10 11:16:27 +0100

    feat: speeding up load sequence

--- index.js
+++ index.js
@@ -3,34 +3,28 @@ const Eris = require('eris')
 require("dotenv").config()
 
 const client = new Eris(process.env.TOKEN)
-let commands = []
+let commandNames = []
 
-client.on('ready', () => {
-    commands = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
-    let loadedCommands = 0
-    for (const command of commands) {
-        try {
-            const commandFile = require(`./commands/${command}`)
-            if(!commandFile.options){
-                client.createCommand({
-                    name: commandFile.name,
-                    description: commandFile.description,
-                    type: commandFile.type
-                })
-            } else {
-                client.createCommand({
-                    name: commandFile.name,
-                    description: commandFile.description,
-                    type: commandFile.type,
-                    options: commandFile.options
-                })
-            }
-            loadedCommands++
-        } catch(err) {
-            console.log(err)
+client.on('ready', async () => {
+    commandNames = fs.readdirSync('./commands').filter(file => file.endsWith('.js'))
+    let commands = []
+    const loadedCommands = await client.getCommands()
+    for (const commandName of commandNames) {
+        const commandFile = require(`./commands/${commandName}`)
+        const loadedCommand = loadedCommands.find(command => command.name === commandFile.name)
+        let command = {
+            name: commandFile.name,
+            description: commandFile.description,
+            type: commandFile.type
         }
+        if(commandFile.options){
+            command.options = commandFile.options
+        }
+        if(loadedCommand && loadedCommand.description === command.description && loadedCommand.options === command.options && loadedCommand.type === command.type) continue;
+        commands.push(command)
     }
-    console.log(`Loaded ${loadedCommands} commands, ready`)
+    if(commands.length > 0) await client.bulkEditCommands(commands)
+    console.log(`Loaded ${(await client.getCommands()).length} commands, ready`)
 })
 
 client.on('interactionCreate', async (interaction) => {
@@ -39,7 +33,7 @@ client.on('interactionCreate', async (interaction) => {
             try {
                 const commandFile = require(`./commands/${command}`)
                 if (commandFile.name === interaction.data.name) {
-                    commandFile.run(client, interaction)
+                    await commandFile.run(client, interaction)
                 }
             } catch(err) {
                 console.log(err)
